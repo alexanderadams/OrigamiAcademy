860a932eb8b0b6fe5d8ab292fae45d160cb5d2ca
Christiano Contreras <nanius05@lima.cs.utexas.edu>
2016-10-16 16:45:08 -0500

    Got Authentication working working for the most part

--- OrigamiAcademy/AuthenticationController.swift
+++ OrigamiAcademy/AuthenticationController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class AuthenticationController: UIViewController {
 
@@ -14,6 +15,7 @@ class AuthenticationController: UIViewController {
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var loginRegisterButton: UIButton!
+    @IBOutlet weak var errorLabel: UILabel!
     
     var newUser:Bool? = nil
     
@@ -41,5 +43,74 @@ class AuthenticationController: UIViewController {
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         self.view.endEditing(true)
+    }
+    
+    @IBAction func loginRegisterButtonClicked(sender: AnyObject) {
+        let userName:String = usernameTextField.text!
+        let password:String = passwordTextField.text!
+        if newUser! {
+            let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+            let managedContext = appDelegate.managedObjectContext
+            let entity = NSEntityDescription.entityForName("User", inManagedObjectContext: managedContext)
+            let user = NSManagedObject(entity: entity!, insertIntoManagedObjectContext: managedContext)
+        
+            user.setValue(userName, forKey: "userName")
+            user.setValue(password, forKey: "password")
+        
+            do {
+                try managedContext.save()
+            } catch {
+                let nserror = error as NSError
+                NSLog("Unresolved error \(nserror), \(nserror.userInfo)")
+                abort()
+            }
+            
+        } else {
+            let user = fetchUser(userName, password: password)
+            if user != nil {
+                let defaults = NSUserDefaults.standardUserDefaults()
+                defaults.setObject(userName, forKey: "loggedInUser")
+                self.performSegueWithIdentifier("loginRegisterSegue", sender: self)
+            } else {
+                errorLabel.hidden = false
+                
+            }
+        }
+        
+    }
+    func fetchUser(userName:String, password:String) -> NSManagedObject? {
+        let fetchedResults = retrieveUsers()
+        var tempUserName:String? = nil
+        var tempPassword:String? = nil
+        
+        for user in fetchedResults {
+            tempUserName = user.valueForKey("userName") as? String
+            tempPassword = user.valueForKey("password") as? String
+            
+            if userName == tempUserName && password == tempPassword {
+                return user
+            }
+        }
+        
+        return nil
+    }
+    
+    func retrieveUsers() -> [NSManagedObject] {
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        let managedContext = appDelegate.managedObjectContext
+        
+        let fetchRequest = NSFetchRequest(entityName: "User")
+        var fetchedResults:[NSManagedObject]? = nil
+        
+        do {
+            try fetchedResults = managedContext.executeFetchRequest(fetchRequest) as? [NSManagedObject]
+        } catch {
+            let nserror = error as NSError
+            NSLog("Unresolved error \(nserror), \(nserror.userInfo)")
+            abort()
+        }
+        
+        return(fetchedResults)!
+        
     }
 }
