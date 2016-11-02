402c7b2552c9c6e04ecd0e3ee6a589ce1f6b7a58
alexanderadams <alexander.adams.mail@gmail.com>
2016-11-01 19:16:28 -0500

    Step editor now correctly gets and saves all step info except for uploaded images

--- OrigamiAcademy/StepEditorController.swift
+++ OrigamiAcademy/StepEditorController.swift
@@ -9,10 +9,12 @@
 import UIKit
 import CoreData
 
-class StepEditorController : UIViewController {
+class StepEditorController : UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     var stepNumber:Int = 0
     var stepObject:NSObject?
+    var imageName:String = String()
+    let imagePicker = UIImagePickerController()
     
     @IBOutlet weak var stepName: UILabel!
     @IBOutlet weak var instructionText: UITextField!
@@ -21,21 +23,49 @@ class StepEditorController : UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // get step object
-        
+        imagePicker.delegate = self
         // fill in text fields with correct attributes
         stepName.text = "Step \(stepNumber)"
+        instructionText.text = stepObject?.valueForKey("details") as? String
+        imageName = stepObject!.valueForKey("image") as! String
+        image.image = UIImage(named:imageName)
     }
     
     @IBAction func saveStep(sender: AnyObject) {
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        let managedContext = appDelegate.managedObjectContext
+        
         // update step object attributes
+        stepObject?.setValue(instructionText.text, forKey: "details")
+        stepObject?.setValue(imageName, forKey: "image")
+        
         // save step object
+        do {
+            try managedContext.save()
+        } catch {
+            let nserror = error as NSError
+            NSLog("Unresolved error \(nserror), \(nserror.userInfo)")
+            abort()
+        }
     }
     
     @IBAction func uploadImage(sender: AnyObject) {
-        // upload image somehow
-        // save image in assets (possibly over previous image)
-        // save image filename to step object
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .PhotoLibrary
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            image.contentMode = .ScaleAspectFit
+            image.image = pickedImage
+        }
+        
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
