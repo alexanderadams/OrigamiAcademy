869a4478a7cab3704150a2fd4db2f83ea2d48762
Christiano Contreas <nanomay94@utexas.edu>
2016-11-29 16:50:15 -0600

    Got rid of dead code and have the downloading of instructions and images working correctly.

--- OrigamiAcademy/MainMenuController.swift
+++ OrigamiAcademy/MainMenuController.swift
@@ -34,9 +34,9 @@ class MainMenuController : UIViewController {
                 }
                 else
                 {
-                    if !self.instructionsInstalled2(curUser!.uid)
+                    if !self.instructionsInstalled(curUser!.uid)
                     {
-                        self.instructionsInstaller2(curUser!.uid)
+                        self.instructionsInstaller(curUser!.uid)
                     }
 
                 }
@@ -44,9 +44,9 @@ class MainMenuController : UIViewController {
             
         } else {
             
-            if !instructionsInstalled2(curUser!.uid)
+            if !instructionsInstalled(curUser!.uid)
             {
-                instructionsInstaller2(curUser!.uid)
+                instructionsInstaller(curUser!.uid)
             }
         }
         super.viewDidLoad()
@@ -79,98 +79,8 @@ class MainMenuController : UIViewController {
         clearImages()
     }
     
-    func instructionsInstalled() -> Bool { 
-        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
-        let managedContext = appDelegate.managedObjectContext
-        
-        let fetchRequest = NSFetchRequest(entityName:"Instruction")
-        var instructionsList:[NSManagedObject]? = nil
-        
-        do {
-            try instructionsList = managedContext.executeFetchRequest(fetchRequest) as? [NSManagedObject]
-        } catch {
-            // If an error occurs
-            let nserror = error as NSError
-            NSLog("Unresolved error \(nserror), \(nserror.userInfo)")
-            abort()
-        }
-        let instructionNumber = Int(NSLocalizedString("instructionNumber", tableName: "instruction_list", comment:"number of instructions"))
-        
-        if instructionsList?.count < instructionNumber {
-            return false
-        }
-        else {
-            for i in 1...instructionNumber! {
-                let creation = NSLocalizedString("instruction\(i)", tableName: "instruction_list", comment:"instruction at index")
-                if !exists(instructionsList!, creation: creation) {
-                    return false
-                }
-            }
-        }
-        return true
-    }
     
-    func exists(list:[NSManagedObject], creation:String) -> Bool {
-        for i in list {
-            if i.valueForKey("creation") as! String == creation {
-                return true
-            }
-        }
-        return false
-    }
-    
-    func instructionsInstaller() {
-        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
-        let managedContext = appDelegate.managedObjectContext
-        
-        let instructionNumber = Int(NSLocalizedString("instructionNumber", tableName: "instruction_list", comment:"number of instructions"))
-        for i in 1...instructionNumber! {
-            
-            // Create the entity we want to save
-            let entity =  NSEntityDescription.entityForName("Instruction", inManagedObjectContext: managedContext)
-            let instruction = NSManagedObject(entity: entity!, insertIntoManagedObjectContext:managedContext)
-            
-            // Set the attribute values
-            let creation = NSLocalizedString("instruction\(i)", tableName: "instruction_list", comment:"creation name")
-            instruction.setValue(creation, forKey: "creation")
-            let stepNumber = Int(NSLocalizedString("stepNumber", tableName: "\(creation)_instructions", comment:"number of steps"))
-            instruction.setValue(stepNumber, forKey: "numOfSteps")
-            let author = NSLocalizedString("author", tableName: "\(creation)_instructions", comment: "author name")
-            instruction.setValue(author, forKey: "author")
-            let summary = NSLocalizedString("description", tableName: "\(creation)_instructions", comment: "creation description")
-            instruction.setValue(summary, forKey: "summary")
-            instruction.setValue("\(creation)_step\(stepNumber)", forKey: "finishedImage")
-            
-            // Create the instruction steps
-            let stepSet:NSMutableOrderedSet = []
-            for s in 1...stepNumber! {
-                
-                let entity =  NSEntityDescription.entityForName("Step", inManagedObjectContext: managedContext)
-                let step = NSManagedObject(entity: entity!, insertIntoManagedObjectContext:managedContext)
-                
-                step.setValue(s, forKey: "number")
-                let details = NSLocalizedString("step\(s)", tableName: "\(creation)_instructions", comment:"step detail")
-                step.setValue(details, forKey: "details")
-                step.setValue("\(creation)_step\(s)", forKey: "image")
-                step.setValue(instruction, forKey: "instruction")
-                stepSet.addObject(step)
-            }
-            let finalSet:NSOrderedSet = stepSet
-            instruction.setValue(finalSet, forKey: "steps")
-        }
-        
-        // Commit the changes
-        do {
-            try managedContext.save()
-        } catch {
-            // If an error occurs
-            let nserror = error as NSError
-            NSLog("Unresolved error \(nserror), \(nserror.userInfo)")
-            abort()
-        }
-    }
-    
-    func instructionsInstalled2(uid:String) -> Bool {
+    func instructionsInstalled(uid:String) -> Bool {
         let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
         let managedContext = appDelegate.managedObjectContext
         
@@ -193,7 +103,7 @@ class MainMenuController : UIViewController {
         return false
     }
     
-    func instructionsInstaller2(uid:String) {
+    func instructionsInstaller(uid:String) {
         
             let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
             let managedContext = appDelegate.managedObjectContext
